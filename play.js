18c46024ae2385ebb51adba47b8194598ad93522
SorrenJ <soalexjao@gmail.com>
2024-04-19 20:03:37 -0700

    Add connect function andevent handler to play.js

---
+++ play.js
@@ -0,0 +1,47 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("Connection is successfully established!");
+    conn.write("Name: Sorren");
+
+  });
+
+  // Update the connect function to handle incoming data and console.log it for the player.
+  conn.on("data", (data) => {
+    console.log("incoming data: ", data);
+  });
+
+
+
+
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+
+
+
+
+
+
+
+
+// Begin game
+game.start()
