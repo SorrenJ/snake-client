4f86f94a7fc0a914144aa9c4dce639f65a394cab
SorrenJ <soalexjao@gmail.com>
2024-04-25 22:33:05 -0700

    implement setupInput and empty handleUserInput function

--- play.js
+++ play.js
@@ -3,10 +3,21 @@ const connect = require("./client");
 
 
 let conn = connect();
+stdin.on("data", handleUserInput);
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+  };
 
 
-
-
+  // specifies what happens when "data:" is received from stdin...  - key inputs
+  const handleUserInput = function () {
+    // your code here
+  };
 
 
 
