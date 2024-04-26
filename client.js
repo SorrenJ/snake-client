bbaf01909ff0c2c76a096edf40c22669d8c7443e
SorrenJ <soalexjao@gmail.com>
2024-04-25 23:57:43 -0700

    constants module

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 const net = require("net");
-
+const { IP, PORT } = require('./constants');
 // establishes a connection with the game server
 const connect = function () {
 
   console.log("Connecting...");
   
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
 
@@ -15,12 +15,13 @@ const connect = function () {
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log("Connection is successfully established!");
+    /*
     conn.write("Name: Sor");
     conn.write("Move: up");
     conn.write("Move: down");
     conn.write("Move: left");
     conn.write("Move: right");
-    
+    */
   });
 
   // Update the connect function to handle incoming data and console.log it for the player.
