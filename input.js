272776d030817ecfe066f1c0d1d32bb4d391ec6f
SorrenJ <soalexjao@gmail.com>
2024-04-25 23:37:24 -0700

    WASD control mapped in input.js

--- input.js
+++ input.js
@@ -1,27 +1,50 @@
 let connection;
 
 
-const setupInput = function (conn) {
-    connection = conn;
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput, setupInput);
-    return stdin;
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput, setupInput);
+  return stdin;
   
 
 };
 
 
-  // specifies what happens when "data:" is received from stdin...  - key inputs
-  const handleUserInput = function (key) {
-    // your code here
-    if (key === '\u0003') {
-        process.exit();
-      }
-  };
+// specifies what happens when "data:" is received from stdin...  - key inputs
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+    
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+    
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+    
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+      
 
-  module.exports = {
-    setupInput
-  };
+  /*
+  "Move: up" - move up one square (unless facing down)
+  "Move: down" - move down one square (unless facing up)
+  "Move: left" - move left one square (unless facing right)
+  "Move: right" - move left one square (unless facing left)
+*/
+};
+
+module.exports = {
+  setupInput
+};
